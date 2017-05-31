b3cf6be723a722b7c36e0b86785e5d54091e1f5f
Marium Askri <mariumaskri@InitiumAdmins-MacBook-Air.local>
2017-05-31 14:35:54 -0400

    Angular cli package updated;mongodump added

--- server/dbApi.js
+++ server/dbApi.js
@@ -9,30 +9,23 @@ var MongoClient = require('mongodb').MongoClient
 
 // Connection URL
 var url = 'mongodb://localhost:27017/searsCa';
+MongoClient.connect(url, function(err, db) {
+  if (err)
+    return res.send({errorMessage: err});
+  assert.equal(null, err);
+  console.log("Connected correctly to mongodb server");
+  router.get('/item/:id', function (req, res) {
+    var itemId = req.params.id;
+    if (itemId === "")
+      return res.send({errorMessage: "Item id cannot be empty"});
 
-router.get('/item/:id',function (req,res) {
-  var itemId = req.params.id;
-  if(itemId === "")
-    return res.send({errorMessage:"Item id cannot be empty"});
-// Use connect method to connect to the Server
-  MongoClient.connect(url, function(err, db) {
-    if(err)
-      return res.send({errorMessage:err});
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    db.collection('ontarioStores').find({item_id:itemId}).toArray(function(err, docs) {
-      console.log(docs);
-      res.send({items:docs});
+    db.collection('ontarioStores').find({item_id: itemId}).toArray(function (err, docs) {
+      res.send({items: docs});
     });
   });
-});
 
-router.get('/itemIds',function (req,res) {
-  MongoClient.connect(url, function(err, db) {
-    if(err)
-      return res.send({errorMessage:err});
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+
+  router.get('/itemIds',function (req,res) {
     db.collection('ontarioStores').distinct("item_id",function (error,docs) {
       if(error)
         return res.send({errorMessage:error});
